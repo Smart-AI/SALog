b80d9ec2d72c5970fbe57db483af3991f9dea43b
Smart-AI <weizhidaoshuo@gmail.com>
2017-10-11 18:45:36 +0800

    Bump version to 1.1.0

--- Example/Example/ViewController.m
+++ Example/Example/ViewController.m
@@ -2,11 +2,12 @@
 //  ViewController.m
 //  Example
 //
-//  Created by zhuwencheng on 2017/10/11.
+//  Created by Smart-AI on 2017/10/11.
 //  Copyright © 2017年 Smart-AI. All rights reserved.
 //
 
 #import "ViewController.h"
+#import "SALog.h"
 
 @interface ViewController ()
 
@@ -17,6 +18,15 @@
 - (void)viewDidLoad {
 	[super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+	
+	NSLog(@"NSLog");
+	
+	SASLog(@"SASLog");
+	
+	SALog(@"SALog\n");
+	
+	SALLog(@"SALLog");
+	
 }
 
 
--- SALog/SALog.h
+++ SALog/SALog.h
@@ -1,13 +1,31 @@
 //
 //  SALog.h
-//  Example
+//  SALog
 //
-//  Created by zhuwencheng on 2017/10/11.
+//  Created by Smart-AI on 2017/10/11.
 //  Copyright © 2017年 Smart-AI. All rights reserved.
 //
 
 #ifndef SALog_h
 #define SALog_h
 
+#ifdef DEBUG
+
+//(SAShortLog) show log
+#define SASLog(fmt, ...) fprintf(stderr, "%s\n", [[NSString stringWithFormat:fmt, ##__VA_ARGS__] UTF8String]);
+
+//(SALog) show function & line number & log
+#define SALog(fmt, ...) fprintf(stderr, "%s-[LineNumber %d] %s\n", [[NSString stringWithUTF8String:__PRETTY_FUNCTION__] UTF8String], __LINE__, [[NSString stringWithFormat:fmt, ##__VA_ARGS__] UTF8String]);
+
+//(SALongLog) show project name & thread number & function & line number & log
+#define SALLog(fmt, ...) NSLog((@"%s-[lineNumber %d] " fmt), __PRETTY_FUNCTION__, __LINE__, ##__VA_ARGS__);
+
+#else
+
+#define SASLog(...)
+#define SALog(...)
+#define SALLog(...)
+
+#endif
 
 #endif /* SALog_h */
