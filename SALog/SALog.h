58fbd30148e4f18e962086f0a8622698c885a668
Smart-AI <weizhidaoshuo@gmail.com>
2017-10-11 18:40:48 +0800

    Bump version to 1.0.0

---
+++ SALog/SALog.h
@@ -0,0 +1,13 @@
+//
+//  SALog.h
+//  Example
+//
+//  Created by zhuwencheng on 2017/10/11.
+//  Copyright © 2017年 Smart-AI. All rights reserved.
+//
+
+#ifndef SALog_h
+#define SALog_h
+
+
+#endif /* SALog_h */
